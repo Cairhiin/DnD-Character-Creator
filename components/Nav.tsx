1d3fa53f2a6e45b112a9ce4b297c515e13153e72
Frank van de Voorde <daemonaz@kymp.net>
2023-04-08 12:56:06 +0300

    Add more styling to nav bar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,7 +4,10 @@ const Nav = () => {
     return (
         <nav className={styles.nav}>
             <div className={styles.logo}>
-                <h1>D<span>&</span>D Character <span>Manager</span></h1>
+                <h1>
+                    D<span className={styles.secondaryColor__highlight}>&</span>D Character 
+                    <span className={styles.secondaryColor__highlight}>Manager</span>
+                </h1>
             </div>
             <div className={styles.auth}>
                 <ul>
