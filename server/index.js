25fc40ca73d271099c9d09a043dc35830b5dd146
Frank van de Voorde <daemonaz@kymp.net>
2023-04-11 10:12:34 +0300

    Add database and user routes

--- server/index.js
+++ server/index.js
@@ -4,16 +4,22 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+const users = require('./routes/users');
+
+mongoose.connect(config.database);
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to database: ${config.database}`);
+});
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
-app.get('/api/', (req, res) => {
-    res.send('API Route')
-});
+app.use('/api/users', users);
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
