456ffcb55984e13150f1c38a6bf90e75ed9671f0
Frank van de Voorde <daemonaz@kymp.net>
2023-04-17 10:18:01 +0300

    Rename directory to src instead of client

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import '@/styles/globals.scss';
+import type { AppProps } from 'next/app';
+import { Roboto_Condensed } from 'next/font/google';
+import { SessionProvider } from 'next-auth/react';
+import Layout from '@/components/Layout';
+import Nav from '@/components/Nav';
+
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={ session }>
+      <Nav />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
+}
