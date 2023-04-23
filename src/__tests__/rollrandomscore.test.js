d811316751824991883ed8400ab70ee0a9c1aad3
Frank van de Voorde <daemonaz@kymp.net>
2023-04-23 13:45:45 +0300

    Rename roll random score test to be inline with naming convention - shame on me

--- src/__tests__/rollrandomscore.test.js
+++ src/__tests__/rollrandomscore.test.js
@@ -5,6 +5,7 @@ beforeEach(() => {
   jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
 });
 
+
 describe('roll random score function', () => {
   test('Roll four dice and drop lowest', () => {
     expect(rollRandomScore()).toBe(12);
