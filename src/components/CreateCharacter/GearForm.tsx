588d26be42bf3b67d8d36d3824e6d027d6a6c996
Frank van de Voorde <daemonaz@kymp.net>
2023-05-19 16:16:34 +0300

    Refactor Gear Form component to be a tad tidier and offer a better UX

--- src/components/CreateCharacter/GearForm.tsx
+++ src/components/CreateCharacter/GearForm.tsx
@@ -129,9 +129,11 @@ export default function GearForm({
             <p>
               Gear:{" "}
               {starting_equipment &&
-                starting_equipment.map((item) => (
+                starting_equipment.map((item, index, arr) => (
                   <span key={item.equipment.index}>
-                    {item.equipment.name},{" "}
+                    {index < arr.length - 1
+                      ? `${item.equipment.name}, `
+                      : item.equipment.name}
                   </span>
                 ))}
             </p>
@@ -145,9 +147,9 @@ export default function GearForm({
         className={styles.character__creation__form}
         onSubmit={handleSubmit(saveData)}
       >
-        <div className={styles.character__creation__form__column}>
+        <div>
           <h3>Choose starting gear</h3>
-          <div>
+          <div className={styles.character__creation__form__column}>
             {starting_equipment_options?.map(
               (option, index): JSX.Element => (
                 <div key={index}>
@@ -158,93 +160,70 @@ export default function GearForm({
                   <div>{option.desc}</div>
                   <div className={styles.character__creation__form__flex}>
                     {option.from.options &&
-                      option.from.options.map((option: any): JSX.Element => {
-                        /* If the option type is a counted reference we can simply add a button
+                      option.from.options.map(
+                        (option: any, optionNumber: number): JSX.Element => {
+                          /* If the option type is a counted reference we can simply add a button
                         and pass the item, otherwise we will have to give the user a dropdown */
-                        if (option.option_type === "counted_reference") {
-                          return (
-                            <div
-                              tabIndex={1234} // necessary to make the div focusable
-                              key={option.of.index}
-                              className={styles.create__form__special__button}
-                              onClick={() =>
-                                addItem(
-                                  [{ item: option.of as Item, amount: 1 }],
-                                  index
-                                )
-                              }
-                            >
-                              {option.of.name}
-                            </div>
-                          );
-                        } else if (option.option_type === "multiple") {
-                          const items: { item: Item; amount: number }[] =
-                            option.items.map((item: any): any => {
-                              if (item.option_type === "counted_reference")
-                                return { item: item.of, amount: item.count };
+                          if (option.option_type === "counted_reference") {
+                            return (
+                              <div
+                                tabIndex={1234} // necessary to make the div focusable
+                                key={option.of.index}
+                                className={styles.create__form__special__button}
+                                onClick={() =>
+                                  addItem(
+                                    [{ item: option.of as Item, amount: 1 }],
+                                    index
+                                  )
+                                }
+                              >
+                                {String.fromCharCode(65 + optionNumber)}
+                              </div>
+                            );
+                          } else if (option.option_type === "multiple") {
+                            const items: { item: Item; amount: number }[] =
+                              option.items.map((item: any): any => {
+                                if (item.option_type === "counted_reference")
+                                  return { item: item.of, amount: item.count };
+                                return {
+                                  item: item.choice.from.equipment_category,
+                                  amount: item.choice.choose,
+                                };
+                              });
+
+                            return (
+                              <div
+                                tabIndex={1234} // necessary to make the div focusable
+                                key={`${option.desc}`}
+                                onClick={() => addItem(items, index)}
+                              >
+                                {String.fromCharCode(65 + optionNumber)}
+                              </div>
+                            );
+                          } else {
+                            /* HACK: Add the number of martial weapons or simple weapons based on the amount
+                            the player gets to choose. Opted for this instead of changing the amount as that indicates
+                            mostly the number of a specific simpleWeapons, not a weapon choice. */
+                            const items: { item: Item; amount: number }[] = [
+                              ...Array(option.choice.choose),
+                            ].map((i: number, count: number): any => {
                               return {
-                                item: item.choice.from.equipment_category,
-                                amount: item.choice.choose,
+                                item: option.choice.from.equipment_category,
+                                amount: 1,
                               };
                             });
 
-                          return (
-                            <div
-                              tabIndex={1234} // necessary to make the div focusable
-                              key={`${option.desc}`}
-                              onClick={() => addItem(items, index)}
-                            >
-                              {items.map(({ item, amount }) => {
-                                if (item.index === "martial-weapons") {
-                                  /* HACK: Adding a number of select elements equal to the options given by the class
-                                  but looks ugly */
-                                  const itemJSX = [...Array(amount)].map(
-                                    (i: number, index: number) => (
-                                      <select key={index}>
-                                        {martialWeapons.map(
-                                          (weapon: Item): JSX.Element => (
-                                            <option key={weapon.index}>
-                                              {weapon.name}
-                                            </option>
-                                          )
-                                        )}
-                                      </select>
-                                    )
-                                  );
-                                  return itemJSX;
-                                } else {
-                                  return <div>{item.name}</div>;
-                                }
-                              })}
-                            </div>
-                          );
-                        } else {
-                          let availableWeapons: Item[] = [];
-                          /* Check if the weapons to choose from belong to the martial melee group (Barbarian)
-                        if not show the simple weapon list - there are no other options */
-                          option.choice.from.equipment_category.index ===
-                          "martial-melee-weapons"
-                            ? (availableWeapons = martialMeleeWeapons)
-                            : (availableWeapons = simpleWeapons);
-                          return (
-                            <div key={`${option.desc}${index}`}>
-                              {[...Array(option.choice.choose)].map(
-                                (i: number, index: number) => (
-                                  <select key={`${option.choice.desc}${index}`}>
-                                    {availableWeapons.map(
-                                      (weapon: Item): JSX.Element => (
-                                        <option key={weapon.index}>
-                                          {weapon.name}
-                                        </option>
-                                      )
-                                    )}
-                                  </select>
-                                )
-                              )}
-                            </div>
-                          );
+                            return (
+                              <div
+                                key={`${option.desc}${index}`}
+                                onClick={() => addItem(items, index)}
+                              >
+                                {String.fromCharCode(65 + optionNumber)}
+                              </div>
+                            );
+                          }
                         }
-                      })}
+                      )}
                   </div>
                 </div>
               )
@@ -252,73 +231,6 @@ export default function GearForm({
           </div>
           <div></div>
         </div>
-
-        {/* <div>
-          <h3>Armors</h3>
-          <select {...register(`armors.${0}` as const)} onChange={onChange}>
-            <option key="no-armor">No armor</option>
-            {availableArmors.map((item: Equipment) => (
-              <option key={item.index} value={item.index}>
-                {item.name}
-              </option>
-            ))}
-          </select>
-          <select {...register("shield")} onChange={onChange}>
-            <option value="" key="no-shield">
-              No shield
-            </option>
-            <option value="shield">Shield</option>
-          </select>
-          
-          <h3>Weapons</h3>
-          {[...Array(4)].map(
-            (i: number, index: number): JSX.Element => (
-              <select
-                {...register(`weapons.${index}` as const)}
-                key={index}
-                onChange={onChange}
-              >
-                <option key="no-weapon">No weapon</option>
-                {!isLoading &&
-                  simpleWeapons.map((item: Equipment) => (
-                    <option key={item.index} value={item.index}>
-                      {item.name}
-                    </option>
-                  ))}
-              </select>
-            )
-          )}
-          <h3>Tools</h3>
-          {[...Array(2)].map(
-            (i: number, index: number): JSX.Element => (
-              <select
-                {...register(`tools.${index}` as const)}
-                key={index}
-                onChange={onChange}
-              >
-                <option key="no-tool">No tool</option>
-                {availableTools.map((item: Equipment) => (
-                  <option key={item.index} value={item.index}>
-                    {item.name}
-                  </option>
-                ))}
-              </select>
-            )
-          )}
-          <h3>Miscellaneous Items</h3>
-          {[...Array(8)].map(
-            (i: number, index: number): JSX.Element => (
-              <select {...register(`misc.${index}` as const)} key={index}>
-                <option key="no-item">No item</option>
-                {availableMisc.map((item: Equipment) => (
-                  <option key={item.index} value={item.index}>
-                    {item.name}
-                  </option>
-                ))}
-              </select>
-            )
-          )}
-        </div> */}
         {
           <div className={styles.character__creation__form__column}>
             {error && <ErrorField error={error} />}
